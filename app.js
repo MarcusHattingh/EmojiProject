8296b9d94f8fb288d37bf49398035b00e17fa33f
Marcus <mhatt2001@icloud.com>
2023-08-28 10:20:26 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const naturalEmoji = document.querySelector('.natural')
+const enhancedEmoji =document.querySelector('.enhanced')
+
+//eventListener
+
+naturalEmoji.addEventListener('click', () => {
+    if (enhancedEmoji.classList.contains('enhanced')) {
+    enhancedEmoji.classList.add('active');
+    naturalEmoji.classList.remove('active');
+    }
+});
+
+enhancedEmoji.addEventListener('click', () => {
+    if(naturalEmoji.classList.add('natural')) {
+    naturalEmoji.classList.add('active');
+    enhancedEmoji.classList.remove('active');
+    }
+});
